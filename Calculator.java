d65bb432a00d99cefb2a95dd021f2a42bc03b6e0
Shreyansh <doshi.shreyansh@yahoo.in>
2020-11-19 22:16:40 +0530

    Changes made with respect to Point 3

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,9 @@
+
 public class Calculator {
     
     public static void main(String[] args){
    
-    String nums="1,2,3";
+    String nums="1,\n,2,3";
     int sum=Add(nums);
     System.out.println("The sum is:" +sum);
 }
@@ -12,7 +13,15 @@ public class Calculator {
        int sum=0;
       for(int i=0;i<nums.length();i++)
       {
-          if(Character.isDigit(nums.charAt(i)))
+          if(nums.charAt(i)==',' && nums.charAt(i+1)=='\n' )
+        {
+            System.out.println("Error!");
+            return 0;
+        }
+       
+         else if(nums.charAt(i)=='\n' || nums.charAt(i)==',')
+            continue;
+         else if(Character.isDigit(nums.charAt(i)))
           {
               sum+=(nums.charAt(i)-'0');
           }
